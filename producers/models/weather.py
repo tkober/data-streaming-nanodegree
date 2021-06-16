4ae9436d9ead844c8b57c395340107693fc184b2
Thorsten Kober <thorsten.kober@softgarden.de>
2021-06-16 17:09:01 +0200

    Implemented Weather Producer

--- producers/models/weather.py
+++ producers/models/weather.py
@@ -1,4 +1,5 @@
 """Methods pertaining to weather data"""
+from dataclasses import dataclass, asdict
 from enum import IntEnum
 import json
 import logging
@@ -13,6 +14,15 @@ from models.producer import Producer
 
 logger = logging.getLogger(__name__)
 
+# Define a Data Transfer Objects so changes to the schema only need to be carried over to one place.
+@dataclass(frozen=True)
+class WeatherUpdateDto:
+    temperature: float
+    status: str
+
+@dataclass(frozen=True)
+class WeatherUpdateKeyDto:
+    timestamp: int
 
 class Weather(Producer):
     """Defines a simulated weather model"""
@@ -30,16 +40,14 @@ class Weather(Producer):
     summer_months = set((6, 7, 8))
 
     def __init__(self, month):
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
+        # Use a versioning so we do not have to setup the docker container once we run into schema conflicts
+        # TODO: Remove in final version
         super().__init__(
-            "weather", # TODO: Come up with a better topic name
+            "weather.update.v1",
             key_schema=Weather.key_schema,
             value_schema=Weather.value_schema,
+            num_partitions= 1,
+            num_replicas=1
         )
 
         self.status = Weather.status.sunny
@@ -53,9 +61,6 @@ class Weather(Producer):
             with open(f"{Path(__file__).parents[0]}/schemas/weather_key.json") as f:
                 Weather.key_schema = json.load(f)
 
-        #
-        # TODO: Define this value schema in `schemas/weather_value.json
-        #
         if Weather.value_schema is None:
             with open(f"{Path(__file__).parents[0]}/schemas/weather_value.json") as f:
                 Weather.value_schema = json.load(f)
@@ -73,37 +78,25 @@ class Weather(Producer):
     def run(self, month):
         self._set_weather(month)
 
-        #
-        #
-        # TODO: Complete the function by posting a weather event to REST Proxy. Make sure to
-        # specify the Avro schemas and verify that you are using the correct Content-Type header.
-        #
-        #
-        logger.info("weather kafka proxy integration incomplete - skipping")
-        #resp = requests.post(
-        #    #
-        #    #
-        #    # TODO: What URL should be POSTed to?
-        #    #
-        #    #
-        #    f"{Weather.rest_proxy_url}/TODO",
-        #    #
-        #    #
-        #    # TODO: What Headers need to bet set?
-        #    #
-        #    #
-        #    headers={"Content-Type": "TODO"},
-        #    data=json.dumps(
-        #        {
-        #            #
-        #            #
-        #            # TODO: Provide key schema, value schema, and records
-        #            #
-        #            #
-        #        }
-        #    ),
-        #)
-        #resp.raise_for_status()
+        keyDto = WeatherUpdateKeyDto(self.time_millis())
+        valueDto = WeatherUpdateDto(temperature=self.temp, status=self.status.name)
+
+        headers = { 'Content-Type': 'application/vnd.kafka.avro.v2+json' }
+        data = {
+            'value_schema': json.dumps(Weather.value_schema),
+            'key_schema': json.dumps(Weather.key_schema),
+            'records': [{
+                'value': asdict(valueDto),
+                'key': asdict(keyDto)
+            }]
+        }
+        url = f'{Weather.rest_proxy_url}/topics/{self.topic_name}'
+        response = requests.post(url=url, data=json.dumps(data), headers=headers)
+        try:
+            response.raise_for_status()
+        except:
+            logger.error(f'Failed to send weather update to Kafka REST Proxy: status -> { response.status_code }\n{ json.dumps(response.json(), indent=2) }')
+            exit(-1)
 
         logger.debug(
             "sent weather data to kafka, temp: %s, status: %s",
