a24b46228f6ee0f226f95b07e2926b4889982c56
Thorsten Kober <thorsten.kober@softgarden.de>
2021-06-17 17:32:12 +0200

    Built base consumer implementation

--- consumers/consumer.py
+++ consumers/consumer.py
@@ -1,8 +1,7 @@
 """Defines core consumer functionality"""
 import logging
 
-import confluent_kafka
-from confluent_kafka import Consumer
+from confluent_kafka import Consumer, OFFSET_BEGINNING
 from confluent_kafka.avro import AvroConsumer
 from confluent_kafka.avro.serializer import SerializerError
 from tornado import gen
@@ -10,6 +9,11 @@ from tornado import gen
 
 logger = logging.getLogger(__name__)
 
+BROKER_URLS = 'PLAINTEXT://localhost:9092'
+# Use this if if you added the other two brokers in the docker-compose file
+#BROKER_URLS = 'PLAINTEXT://localhost:9092,PLAINTEXT://localhost:9093,PLAINTEXT://localhost:9094'
+SCHEMA_REGISTRY_URL = 'http://localhost:8081'
+CLIENT_TIMEOUT = 3
 
 class KafkaConsumer:
     """Defines the base kafka consumer class"""
@@ -30,46 +34,28 @@ class KafkaConsumer:
         self.consume_timeout = consume_timeout
         self.offset_earliest = offset_earliest
 
-        #
-        #
-        # TODO: Configure the broker properties below. Make sure to reference the project README
-        # and use the Host URL for Kafka and Schema Registry!
-        #
-        #
         self.broker_properties = {
-                #
-                # TODO
-                #
+            'bootstrap.servers': BROKER_URLS,
+            'group.id': f'{self.topic_name_pattern}.group.0'
         }
 
-        # TODO: Create the Consumer, using the appropriate type.
-        if is_avro is True:
-            self.broker_properties["schema.registry.url"] = "http://localhost:8081"
-            #self.consumer = AvroConsumer(...)
-        else:
-            #self.consumer = Consumer(...)
-            pass
+        if self.offset_earliest:
+            self.broker_properties['auto.offset.reset'] = 'earliest'
 
-        #
-        #
-        # TODO: Configure the AvroConsumer and subscribe to the topics. Make sure to think about
-        # how the `on_assign` callback should be invoked.
-        #
-        #
-        # self.consumer.subscribe( TODO )
+        if is_avro is True:
+            self.broker_properties['schema.registry.url'] = SCHEMA_REGISTRY_URL
+            self.consumer = AvroConsumer(config=self.broker_properties)
+        else:
+            self.consumer = Consumer(config=self.broker_properties)
+
+        self.consumer.subscribe([self.topic_name_pattern], on_assign=self.on_assign)
 
     def on_assign(self, consumer, partitions):
         """Callback for when topic assignment takes place"""
-        # TODO: If the topic is configured to use `offset_earliest` set the partition offset to
-        # the beginning or earliest
-        logger.info("on_assign is incomplete - skipping")
-        for partition in partitions:
-            pass
-            #
-            #
-            # TODO
-            #
-            #
+
+        if self.offset_earliest:
+            for partition in partitions:
+                partition.offset = OFFSET_BEGINNING
 
         logger.info("partitions assigned for %s", self.topic_name_pattern)
         consumer.assign(partitions)
@@ -84,21 +70,26 @@ class KafkaConsumer:
 
     def _consume(self):
         """Polls for a message. Returns 1 if a message was received, 0 otherwise"""
-        #
-        #
-        # TODO: Poll Kafka for messages. Make sure to handle any errors or exceptions.
-        # Additionally, make sure you return 1 when a message is processed, and 0 when no message
-        # is retrieved.
-        #
-        #
-        logger.info("_consume is incomplete - skipping")
-        return 0
+
+        try:
+            message = self.consumer.poll(self.consume_timeout)
+
+            if message is None:
+                return 0
+
+            if message.error() is not None:
+                logger.error(f'Error occurred on consumer for topic "{self.topic_name_pattern}":\n{message.error()}')
+                return 0
+
+            self.message_handler(message)
+            return 1
+
+        except (RuntimeError, SerializerError) as e:
+            logger.error(f'Ciritical error occurred during poll for topic "{self.topic_name_pattern}":\n{e}')
+            exit(-1)
 
 
     def close(self):
         """Cleans up any open kafka consumers"""
-        #
-        #
-        # TODO: Cleanup the kafka consumer
-        #
-        #
+        self.consumer.close()
+        logger.info(f'Closed consumer for topic "{self.topic_name_pattern}"')
