215ebab209986adcce39bc6588afd286c67b5fc9
Thorsten Kober <thorsten.kober@softgarden.de>
2021-06-17 16:00:43 +0200

    built faust stream processing

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -1,8 +1,8 @@
 """Defines trends calculations for stations"""
 import logging
+from dataclasses import dataclass
 
 import faust
-
 
 logger = logging.getLogger(__name__)
 
@@ -22,6 +22,7 @@ class Station(faust.Record):
 
 
 # Faust will produce records to Kafka in this format
+@dataclass
 class TransformedStation(faust.Record):
     station_id: int
     station_name: str
@@ -29,29 +30,62 @@ class TransformedStation(faust.Record):
     line: str
 
 
-# TODO: Define a Faust Stream that ingests data from the Kafka Connect stations topic and
-#   places it into a new topic with only the necessary information.
-app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
-# TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
-# TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
-# TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+CONNECT_TOPIC_NAME = 'cta.information.stations'
+TRANSFORMED_TOPIC_NAME = f'{CONNECT_TOPIC_NAME}.transformed'
 
+app = faust.App(
+    "stations-stream",
+    broker="kafka://localhost:9092",
+    store="memory://"
+)
 
-#
-#
-# TODO: Using Faust, transform input `Station` records into `TransformedStation` records. Note that
-# "line" is the color of the station. So if the `Station` record has the field `red` set to true,
-# then you would set the `line` of the `TransformedStation` record to the string `"red"`
-#
-#
+station_information_topic = app.topic(
+    CONNECT_TOPIC_NAME,
+    value_type=Station
+)
+
+transformed_topic = app.topic(
+    TRANSFORMED_TOPIC_NAME,
+    partitions=1,
+    value_type=TransformedStation
+)
+
+table = app.Table(
+    'stations_table',
+    default=TransformedStation,
+    partitions=1,
+    changelog_topic=transformed_topic
+)
+
+def lineFromStation(station: Station) -> str:
+    if station.red:
+        return 'red'
+
+    if station.blue:
+        return 'blue'
+
+    if station.green:
+        return 'green'
+
+    return '-'
+
+@app.agent(station_information_topic)
+async def stationInformation(stations):
+    async for station in stations:
+
+        # Determine the line
+        line = lineFromStation(station)
+
+        # Transform the information
+        transformedStation = TransformedStation(
+            station_id=station.station_id,
+            station_name=station.station_name,
+            order=station.order,
+            line=line
+        )
+
+        # Store in the table
+        table[station.station_id] = transformedStation
 
 
 if __name__ == "__main__":
